a8baa808a3308f8358fa2e13af9d0fc6b26a6a9e
AdhiVenkatesh <aadhi494@gmail.com>
2024-08-26 00:37:26 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "https://blog-posts-mauve.vercel.app/" }));
 app.use(express.json());
 
 mongoose
